e04f778b7f03a8e1b87c1763c4b069be0c327556
Avani joshi <avaniklsjoshi@gmail.com>
2017-08-15 14:56:43 +0530

    backend done

---
+++ routes/tasks.js
@@ -0,0 +1,77 @@
+/* Created by avani on 17-06-2017. */
+var express                 = require('express');
+var router                  = express.Router();
+var mongojs                 = require('mongojs');
+var db                      = mongojs('mongodb://gudiya:1234@ds127962.mlab.com:27962/avani',['tasks']);  //collection
+
+//get all tasks
+router.get('/tasks', function (req,res,next) {
+   // res.send('TASK API');
+    db.tasks.find(function (err,tasks) {
+       if(err)
+        res.send(err);
+       res.json(tasks);
+    });
+});
+
+//get single task
+router.get('/task/:id', function (req,res,next) {
+    // res.send('TASK API');
+    db.tasks.findOne({_id:mongojs.ObjectID(req.params.id)},function (err,task) {
+        if(err)
+            res.send(err);
+        res.json(task);
+    });
+});
+
+//save task
+router.post('/task', function (req,res,next) {
+  var task = req.body;
+  if(!task.title || (task.isDone + '')){
+    res.status(400);
+    res.json({"error" : "bad data.."});
+  } else {
+    db.tasks.save(task,function (err,task) {
+      if(err)
+        res.send(err);
+      res.json(task);
+    });
+  }
+});
+
+//delete task
+router.delete('/tasks/:id', function (req,res,next) {
+    // res.send('TASK API');
+    db.tasks.remove({_id:     mongojs.ObjectID(req.params.id)},function (err,task) {
+        if(err)
+            res.send(err);
+        res.json(task);
+    });
+});
+
+//update task
+router.put('/tasks/:id', function (req,res,next) {
+    var task                = req.body;
+    var updTask             = {};
+
+    if(task.isDone){
+        updTask.isDone      = task.isDone;
+    }
+    
+    if(task.title){
+        updTask.title       = task.title
+    }
+    
+    if(!updTask) {
+        res.status(400);
+        res.json({"error":    "bad data."});
+    } else {
+        db.tasks.update({_id: mongojs.ObjectID(req.params.id)},updTask,{},function (err,task) {
+        if(err)
+            res.send(err);
+        res.json(task);
+        })
+    }
+});
+
+module.exports              = router;
---
+++ server.js
@@ -0,0 +1,28 @@
+var express= require('express');  //
+var path=require('path');  //system module,that is y v dont have to install it, it helps in working with file system paths 
+var bodyParser=require('body-parser'); //accept data from form and parse it and send to db
+
+var index= require('./routes/index');
+var tasks=require('./routes/tasks');  //template system
+
+port=3000;
+var app=express();
+
+//======view engine====================
+app.set('views',path.join(__dirname,'views'));  //our views in views folder
+app.set('view engine','ejs'); //we wanna use ejs
+app.engine('html',require('ejs').renderFile);  //we wanna render file with .html extension
+
+// set static folder for ang 2 ==================
+app.use(express.static(path.join(__dirname,'client')));
+
+// body-parser middleware ======================
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/', index);
+app.use('/api',tasks);
+
+app.listen(port,function(){
+  console.log('Server started on '+port);
+});
